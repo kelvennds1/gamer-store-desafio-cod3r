e53ceb5efe63e34993ebf73ec1ea776fc40be581
Kelven Silva <kelvencrstn@gmail.com>
2024-10-03 03:18:36 -0300

    feat: vesão incial da pagina do produto

---
+++ frontend/src/app/product/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { products } from "@/core";
+
+export default function ProductPage(props: any) {
+  const id = +props.params.id;
+  const product = products.find((p) => p.id === id);
+  return product ? (
+    <div>{product.name}</div>
+  ) : (
+    <div>Produto não encontrado.</div>
+  );
+}
